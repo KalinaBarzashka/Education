cf44a62c9070793179efde4ee6dd6b38d18f0ced
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 17:45:53 +0000

    Wed, Jul 27, 2022, 5:45 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,10 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
+    if (argc != 2)
+    {
+        return 1;
+    }
     string plain_text = get_string("plaintext:  ");
     string cipher_text = plain_text;
+    int length = strlen(plain_text);
 
-    printf("ciphertext: %s", cipher_text);
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+
+    printf("ciphertext: %s\n", cipher_text);
 }
