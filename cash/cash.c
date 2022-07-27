27517639a024d0c76d66c0d7c5288bb22541d9ea
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 08:27:23 +0000

    Wed, Jul 27, 2022, 8:27 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -11,6 +11,7 @@ int main(void)
 {
     // Ask how many cents the customer is owed
     int cents = get_cents();
+    printf("%i", cents);
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
@@ -37,8 +38,14 @@ int main(void)
 
 int get_cents(void)
 {
-    // TODO
-    return 0;
+    int cents;
+    do
+    {
+       cents = get_int("Change owed: ");
+    }
+    while(cents <= 0);
+
+    return cents;
 }
 
 int calculate_quarters(int cents)
