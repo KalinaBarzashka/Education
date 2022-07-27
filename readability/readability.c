4fc78a72ace6bfa4b1fb274d746efea79a801431
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 16:44:19 +0000

    Wed, Jul 27, 2022, 4:44 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int count_letters(string text);
 
@@ -9,9 +11,21 @@ int main(void)
     int letters = count_letters(text);
     int words = 0;
     int sentences = 0;
+
+    printf("%i\n", letters);
 }
 
 int count_letters(string text)
 {
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        char symbol = text[i];
+        if (isalpha(symbol))
+        {
+            count++;
+        }
+    }
 
+    return count;
 }
