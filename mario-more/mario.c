f1488c6745adffac9d41b8767e96cff081f7c19d
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 07:36:58 +0000

    Wed, Jul 27, 2022, 7:36 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = 1;
+
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while(height <= 0 || height > 8);
+
+
 }
