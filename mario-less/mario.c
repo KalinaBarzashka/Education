06c73724bd176bf203350fe136c9e602bd6b42a0
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 08:48:56 +0000

    Wed, Jul 27, 2022, 8:48 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,28 +4,30 @@
 int main(void)
 {
     int height = 1;
+    //ask for height of pyramid; between 1 and 8
     do
     {
         height = get_int("Height: ");
     }
-    while(height <= 0 || height > 8);
+    while (height <= 0 || height > 8);
 
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         int spaces = height - i - 1;
         int dashes = i + 1;
 
         //cycle for spaces
-        for(int j = 0; j < spaces; j++)
+        for (int j = 0; j < spaces; j++)
         {
             printf(" ");
         }
         //cycle for dashes
-        for(int j = 0; j < dashes; j++)
+        for (int j = 0; j < dashes; j++)
         {
             printf("#");
         }
 
+        //print new line
         printf("\n");
     }
 }
