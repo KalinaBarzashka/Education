6c2f97aa478054cb68663e77e4a7e79c9ee9eb68
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 17:47:42 +0000

    Wed, Jul 27, 2022, 5:47 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -8,13 +8,14 @@ int main(int argc, string argv[])
     {
         return 1;
     }
+    else if (argc[1])
     string plain_text = get_string("plaintext:  ");
     string cipher_text = plain_text;
     int length = strlen(plain_text);
 
     for (int i = 0; i < length; i++)
     {
-        
+
     }
 
     printf("ciphertext: %s\n", cipher_text);
