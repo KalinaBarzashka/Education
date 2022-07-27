c39f4976c77d72b5cccaf6c1f4cfefc2af791773
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 07:08:10 +0000

    Wed, Jul 27, 2022, 7:08 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,9 +6,13 @@ int main(void)
     int height = 0;
     do
     {
-        height = get_int("Enter positive height of the pyramid between 1 and 8: ");
+        height = get_int("Height: ");
     }
     while(height <= 0 || height > 8);
 
-    printf("Height is: %i \n", height);
+    for(int i = 0; i < height; i++)
+    {
+        
+        printf("#\n");
+    }
 }
