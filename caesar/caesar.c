03f3b38cdda3220cb39f500b3fd140a2de312046
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 17:52:14 +0000

    Wed, Jul 27, 2022, 5:52 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,10 +6,18 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./caesar key");  
+        printf("Usage: ./caesar key");
         return 1;
     }
-    else if (argc[1])
+
+    int key_length = strlen(argv[1]);
+    string key = argv[1];
+    if(!check_key)
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+
     string plain_text = get_string("plaintext:  ");
     string cipher_text = plain_text;
     int length = strlen(plain_text);
@@ -20,4 +28,18 @@ int main(int argc, string argv[])
     }
 
     printf("ciphertext: %s\n", cipher_text);
+}
+
+bool check_key(string key, int key_length)
+{
+    for (int i = 0; i < key_length; i++)
+    {
+        if (isalpha(key[i]))
+        {
+            printf("Usage: ./caesar key");
+            return false;
+        }
+    }
+
+    return true;
 }
