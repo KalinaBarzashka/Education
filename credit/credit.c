a2c6b58e4c69f06e5e0dc3873c32a9cff286ce04
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 08:56:29 +0000

    Wed, Jul 27, 2022, 8:56 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-    //American Express - 15-digit numbers
-    //MasterCard - 16-digit numbers
-    //Visa - 13 and 16-digit numbers
+    //American Express - 15-digit numbers - starts with 34 or 37
+    //MasterCard - 16-digit numbers - starts with 51, 52, 53, 54, or 55
+    //Visa - 13 and 16-digit numbers - 4
+    //checksum
 }
