febf572997cf1a643b32a1167cf3a9e3edc947ae
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 14:42:50 +0000

    Wed, Jul 27, 2022, 2:42 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,10 +18,23 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    // Print the winner
+    if (score1 == score2)
+    {
+        printf("Tie!");
+    }
+    else if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else
+    {
+        printf("Player 2 wins!");
+    }
 }
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+    // Compute and return score for string
+    
 }
