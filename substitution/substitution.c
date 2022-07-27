36c50698a708bdac60ea3376608e67e0208eb3ac
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 18:42:46 +0000

    Wed, Jul 27, 2022, 6:42 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    //check if there are only 2 arguments: program name and a key
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
 }
