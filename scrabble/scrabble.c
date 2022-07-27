8814a51d7811acb3902560695b60a9d92fa1ac12
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 14:45:53 +0000

    Wed, Jul 27, 2022, 2:45 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -36,5 +36,13 @@ int main(void)
 int compute_score(string word)
 {
     // Compute and return score for string
-    
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (islower(word[i]))
+        {
+            word[i] = toupper(word[i]);
+        }
+    }
+
+    printf("%s", word);
 }
