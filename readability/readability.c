78a9a86305d477f9b626c3ef406825760269bbf9
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 17:16:47 +0000

    Wed, Jul 27, 2022, 5:16 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,8 +11,11 @@ int count_sentences(string text);
 int main(void)
 {
     string text = get_string("Text: ");
+    //get letters count from text
     int letters = count_letters(text);
+    //get words count from text
     int words = count_words(text);
+    //get sentences count from text
     int sentences = count_sentences(text);
 
     // average letters per words
@@ -20,6 +23,7 @@ int main(void)
     // average sentences per words
     double s = (sentences / (float)words) * 100;
 
+    //use Coleman-Liau index
     int index = round(0.0588 * l - 0.296 * s - 15.8);
 
     if (index < 1)
@@ -42,6 +46,7 @@ int count_letters(string text)
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         char symbol = text[i];
+        //check if symbol is a letter
         if (isalpha(symbol))
         {
             count++;
@@ -58,17 +63,19 @@ int count_words(string text)
     for (int i = 0; i < n; i++)
     {
         char symbol = text[i];
+        //check if symbol is a space(32) and if the symbol before was not a space
         if ((int)symbol == 32 && !((int)text[i - 1] == 32))
         {
             count++;
         }
     }
 
+    //check if text starts with space and if it does substract one
     if (text[0] == ' ')
     {
         count--;
     }
-
+    //check if text ends with space and if it does substract one
     if (text[n - 1] == ' ')
     {
         count--;
@@ -84,6 +91,7 @@ int count_sentences(string text)
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         char symbol = text[i];
+        //check if symbol is a !(33), ?(63) or .(46)
         if (symbol == 33 || symbol == 63 || symbol == 46)
         {
             count++;
