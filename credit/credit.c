6a0f2d809b55fcedc8340ce433151cf0504ca1ad
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-27 09:05:55 +0000

    Wed, Jul 27, 2022, 9:05 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,10 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool checksum(long number);
+
 int main(void)
 {
     //American Express - 15-digit numbers - starts with 34 or 37
     //MasterCard - 16-digit numbers - starts with 51, 52, 53, 54, or 55
     //Visa - 13 and 16-digit numbers - 4
     //checksum
+
+    long card_number = get_long("Number: ");
+    bool valid = checksum(number);
+
+    if (!valid)
+    {
+        printf("INVALID\n");
+    }
+    else
+    {
+        printf("VALID");
+    }
+}
+
+bool checksum(long number)
+{
+    
 }
