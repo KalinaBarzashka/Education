601ed36c81d30048a3cc19275e4513a28b99541e
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-29 13:33:51 +0000

    Fri, Jul 29, 2022, 1:33 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -211,6 +211,13 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!candidates[i].eliminated && candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
+
     return;
 }
