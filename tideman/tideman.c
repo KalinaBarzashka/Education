2f777675bf0e21a0365c5a9ba1565c80f9059210
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-07-30 13:32:54 +0000

    Sat, Jul 30, 2022, 1:32 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -142,12 +142,12 @@ void add_pairs(void)
         {
             if (preferences[i][j] > preferences[j][i])
             {
-                pairs[pair_count] 
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
                 pair_count++;
             }
         }
     }
-
 
     return;
 }
