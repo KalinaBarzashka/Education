d99de3c18ec7c42f144bf60c1abe2a0c6192e4ee
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-01 08:24:06 +0000

    Mon, Aug 1, 2022, 8:24 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -165,7 +165,11 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
+
     return;
 }
 
