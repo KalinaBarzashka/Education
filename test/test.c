b42161371324544cafabd893df5d017d85dc86db
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-01 19:52:55 +0000

    Mon, Aug 1, 2022, 7:52 PM +00:00

--- test/test.c
+++ test/test.c
@@ -3,13 +3,7 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 2)
-    {
-        printf("Hello, %s\n", argv[1]);
-    }
-    else
-    {
-        printf("Hello, world\n");
-    }
+    char *s = "HI!";
+    printf("%c\n", *(s+2));
 
 }
