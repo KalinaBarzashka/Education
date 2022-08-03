e044ab4f0d9aeb573acf6cebd2cad560377ac9d9
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-03 10:19:59 +0000

    Wed, Aug 3, 2022, 10:19 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -40,6 +40,19 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
             int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
 
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            else if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            else if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
             image[h][w].rgbtBlue = sepiaBlue;
             image[h][w].rgbtGreen = sepiaGreen;
             image[h][w].rgbtRed = sepiaRed;
