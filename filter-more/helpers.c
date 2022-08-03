27d0e9228830a6851d8f3e1b04e1b30d8e572090
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-03 11:59:50 +0000

    Wed, Aug 3, 2022, 11:59 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -159,6 +159,114 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    //make a cory of the image
+    RGBTRIPLE img_copy[height][width];
+    make_copy(height, width, image, img_copy);
+
+    //each row
+    for (int h = 0; h < height; h++)
+    {
+        //each column / pixel
+        for (int w = 0; w < width; w++)
+        {
+            //rgbtBlue/rgbtGreen/rgbtRed
+            double counter = 0.0;
+            //new avg colors
+            int avgRed = 0;
+            int avgGreen = 0;
+            int avgBlue = 0;
+
+            //add_to_avg(h, w, img_copy, &avgRed, &avgGreen, &avgBlue, &counter);
+            RGBTRIPLE crtPixel = img_copy[h][w];
+            avgRed += crtPixel.rgbtRed;
+            avgGreen += crtPixel.rgbtGreen;
+            avgBlue += crtPixel.rgbtBlue;
+            counter++;
+
+            //top and bottom row pixels
+            if (h - 1 >= 0) //check if we have top row
+            {
+                RGBTRIPLE topMiddle = img_copy[h - 1][w];
+                avgRed += topMiddle.rgbtRed;
+                avgGreen += topMiddle.rgbtGreen;
+                avgBlue += topMiddle.rgbtBlue;
+                counter++;
+
+                if (w - 1 >= 0) //check if we have top left pixel
+                {
+                    RGBTRIPLE topLeft = img_copy[h - 1][w - 1];
+                    avgRed += topLeft.rgbtRed;
+                    avgGreen += topLeft.rgbtGreen;
+                    avgBlue += topLeft.rgbtBlue;
+                    counter++;
+                }
+
+                if (w + 1 < width) //check if we have top right pixel
+                {
+                    RGBTRIPLE topRight = img_copy[h - 1][w + 1];
+                    avgRed += topRight.rgbtRed;
+                    avgGreen += topRight.rgbtGreen;
+                    avgBlue += topRight.rgbtBlue;
+                    counter++;
+                }
+            }
+
+            if (h + 1 < height) //check if we have bottom row
+            {
+                RGBTRIPLE bottomMiddle = img_copy[h + 1][w];
+                avgRed += bottomMiddle.rgbtRed;
+                avgGreen += bottomMiddle.rgbtGreen;
+                avgBlue += bottomMiddle.rgbtBlue;
+                counter++;
+
+                if (w - 1 >= 0) //check if we have bottom left pixel
+                {
+                    RGBTRIPLE bottomLeft = img_copy[h + 1][w - 1];
+                    avgRed += bottomLeft.rgbtRed;
+                    avgGreen += bottomLeft.rgbtGreen;
+                    avgBlue += bottomLeft.rgbtBlue;
+                    counter++;
+                }
+
+                if (w + 1 < width) //check if we have top right pixel
+                {
+                    RGBTRIPLE bottomRight = img_copy[h + 1][w + 1];
+                    avgRed += bottomRight.rgbtRed;
+                    avgGreen += bottomRight.rgbtGreen;
+                    avgBlue += bottomRight.rgbtBlue;
+                    counter++;
+                }
+            }
+
+            //right and left current row pixels
+            if (w + 1 < width)
+            {
+                RGBTRIPLE crtRight = img_copy[h][w + 1];
+                avgRed += crtRight.rgbtRed;
+                avgGreen += crtRight.rgbtGreen;
+                avgBlue += crtRight.rgbtBlue;
+                counter++;
+            }
+
+            if (w - 1 >= 0)
+            {
+                RGBTRIPLE crtLeft = img_copy[h][w - 1];
+                avgRed += crtLeft.rgbtRed;
+                avgGreen += crtLeft.rgbtGreen;
+                avgBlue += crtLeft.rgbtBlue;
+                counter++;
+            }
+
+            avgRed = round(avgRed / counter);
+            avgGreen = round(avgGreen / counter);
+            avgBlue = round(avgBlue / counter);
+
+            image[h][w].rgbtRed = avgRed;
+            image[h][w].rgbtGreen = avgGreen;
+            image[h][w].rgbtBlue = avgBlue;
+        }
+    }
+
     return;
 }
 
