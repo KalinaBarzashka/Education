11faada37d536e737c9ebfd973f55fddd6b9deb2
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-11 14:04:21 +0000

    ddd, Aug 8/11/2022, YYYY, 2:04 A +0

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -60,10 +60,9 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
-    while(len(teams) > 1):
-        simulate_round(teams)
-    return teams["team"]
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 if __name__ == "__main__":
     main()
