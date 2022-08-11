b921571d3590e57090d7e05782b26d850c9f78e1
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-11 16:44:53 +0000

    ddd, Aug 8/11/2022, YYYY, 4:44 A +0

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,9 +1,31 @@
 from cs50 import get_float
 
-change = get_float("Change owed: ")
-coins = 0
 
-while change < 1:
+
+def main():
+    coins = 0
     change = get_float("Change owed: ")
 
-print(change)
+    # if change is negative value, re-promt user for change until its positive float value
+    while change < 0:
+        change = get_float("Change owed: ")
+
+    coins += calculate_quarters(change)
+    coins += calculate_dimes(change)
+    coins += calculate_nickels(change)
+    coins += calculate_pennies(change)
+    print(coins)
+
+def calculate_quarters(change):
+    return change / 0.25
+
+def calculate_dimes(change):
+    return change / 0.10
+
+def calculate_nickels(change):
+    return change / 0.05
+
+def calculate_pennies(change):
+    return change / 0.01
+
+main()
