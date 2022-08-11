fa1bfe88f2755ec1913a18cc27999d94e1adf8d1
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-11 14:27:44 +0000

    ddd, Aug 8/11/2022, YYYY, 2:27 A +0

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -4,7 +4,9 @@ height = 0
 while height < 1 or height > 8:
     height = get_int("Height: ")
 
-for i in range(4):
-    for j in range(4):
+for i in range(height):
+    for j in range(height-i-1):
+        print(" ", end="")
+    for j in range(i+1):
         print("#", end="")
     print()
