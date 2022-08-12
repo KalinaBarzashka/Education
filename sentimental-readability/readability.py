1a4dec62dbede3271e295059149633040823a7d7
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-12 09:54:01 +0000

    ddd, Aug 8/12/2022, YYYY, 9:54 A +0

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -10,9 +10,19 @@ def main():
     # get sentences count from text
     sentences = count_sentences(text)
 
-    print(letters)
-    print(words)
-    print(sentences)
+    # average letters per words
+    l = (letters / float(words)) * 100
+    # average sentences per words
+    s = (sentences / float(words)) * 100
+    # use Coleman-Liau index
+    index = round(0.0588 * l - 0.296 * s - 15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif index > 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
 
 
 def count_letters(text):
@@ -51,14 +61,12 @@ def count_sentences(text):
     count = 0
     for i in range(len(text)):
         symbol = text[i]
-        print(symbol)
         # check if symbol is a !(33), ?(63) or .(46)
-        has_exclam_mark = re.search("\!", symbol)
-        has_quest_mark = re.search("\?", symbol)
-        has_dot = re.search("\.", symbol)
-        has
+        exclam_mark = symbol.count('!')
+        quest_mark = symbol.count('?')
+        dot = symbol.count('.')
 
-        if has_exclam_mark == True or has_quest_mark == True or has_dot == True:
+        if exclam_mark == 1 or quest_mark == 1 or dot == 1:
             count += 1
     return count
 
