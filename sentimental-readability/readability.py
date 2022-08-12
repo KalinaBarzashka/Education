98a217ee2f4666d069f879b02f3a01c5c2d47b9e
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-12 09:45:42 +0000

    ddd, Aug 8/12/2022, YYYY, 9:45 A +0

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,11 +4,15 @@ import re
 def main():
     text = get_string("Text: ")
     # get letters count from text
-    letters = count_letters(text);
+    letters = count_letters(text)
     # get words count from text
-    words = count_words(text);
+    words = count_words(text)
     # get sentences count from text
-    sentences = count_sentences(text);
+    sentences = count_sentences(text)
+
+    print(letters)
+    print(words)
+    print(sentences)
 
 
 def count_letters(text):
@@ -24,7 +28,8 @@ def count_letters(text):
 
 def count_words(text):
     count = 0
-    for i in range(len(text)):
+    lenght = len(text)
+    for i in range(lenght):
         symbol = text[i]
         prev_symbol = "" if i==0 else text[i-1]
         # check if symbol is a space(32) and if the symbol before was not a space
@@ -35,7 +40,7 @@ def count_words(text):
     if text[0].isspace():
         count -= 1
     # check if text ends with space and if it does substract one
-    if text[n - 1].isspace():
+    if text[lenght-1].isspace():
         count -= 1
 
     # return count + 1 for the last word in the sentance
