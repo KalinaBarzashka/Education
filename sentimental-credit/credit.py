0f1874f106829399d690bb8c4caa1d3e581d704a
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-12 06:18:27 +0000

    ddd, Aug 8/12/2022, YYYY, 6:18 A +0

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,7 +1,7 @@
-#American Express - 15-digit numbers - starts with 34 or 37
-#MasterCard - 16-digit numbers - starts with 51, 52, 53, 54, or 55
-#Visa - 13 and 16-digit numbers - 4
-#checksum
+# American Express - 15-digit numbers - starts with 34 or 37
+# MasterCard - 16-digit numbers - starts with 51, 52, 53, 54, or 55
+# Visa - 13 and 16-digit numbers - 4
+# checksum
 
 import re
 from cs50 import get_string
