ec6cd40206564572a2401274fc9ff544ca9228fe
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-15 19:44:41 +0000

    Mon, Aug 15, 2022, 7:44 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,1 +1,5 @@
-SELECT title FROM movies WHERE id = (SELECT movie_id FROM (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = "Johnny Depp") WHERE movie_id = (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = "Helena Bonham Carter"));
+SELECT title FROM movies
+JOIN 
+WHERE id = (SELECT movie_id FROM
+                (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = "Johnny Depp")
+                WHERE movie_id = (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = "Helena Bonham Carter"));
