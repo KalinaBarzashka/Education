b2c0d03c43ca2cb35565edafaf7737dcaf730c25
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-15 19:26:36 +0000

    Mon, Aug 15, 2022, 7:26 PM +00:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,1 @@
-SELECT DISTINCT(people.name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE year = 2014 ORDER BY birth;
+SELECT DISTINCT(people.name) FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON stars.movie_id = movies.id WHERE year = 2004 ORDER BY birth;
