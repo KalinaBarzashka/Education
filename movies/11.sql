0268ce5eefe6f350235215d2f4e5911f5ff86ccc
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-15 19:30:40 +0000

    Mon, Aug 15, 2022, 7:30 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies LIMIT 5;
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id JOIN stars ON movies.id = stars.movie_id where person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman") ORDER BY rating DESC LIMIT 5;
---
+++ movies/12.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT(title) FROM movies JOIN stars ON movies.id = stars.movie_id WHERE person_id in (SELECT id FROM people WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter");
