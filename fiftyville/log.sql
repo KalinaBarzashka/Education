800f8817de5505a2a5ed163a8f0ea9a4dfb23a05
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-16 08:42:37 +0000

    Tue, Aug 16, 2022, 8:42 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
--- SELECT id and description of crime scene reports - found out how many intervies were taken
+-- SELECT id and description of crime scene reports - found out how many intervies were taken; 10:15
 SELECT id, description FROM crime_scene_reports WHERE year = 2021 AND month = 07 AND day = 28 AND street = 'Humphrey Street';
 -- Find more information from the interviews; within 10 minutes of the teft, the thief escape with a car; get footage; ATM on Leggett Street; flight on 29/07/2021
 SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 07 AND day = 28;
