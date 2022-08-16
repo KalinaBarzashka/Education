5882523cebcb67124ef3947628ef865d6d8a963a
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-16 08:43:47 +0000

    Tue, Aug 16, 2022, 8:43 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,7 +3,7 @@ SELECT id, description FROM crime_scene_reports WHERE year = 2021 AND month = 07
 -- Find more information from the interviews; within 10 minutes of the teft, the thief escape with a car; get footage; ATM on Leggett Street; flight on 29/07/2021
 SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 07 AND day = 28;
 -- Check ATM transactions
-SELECT id, account_number, transaction_type, amount FROM atm_transactions WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = 'Leggett Street';
+SELECT id, account_number, transaction_type, amount FROM atm_transactions WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw';
 -- CHECK bank accounts of the previously found ATM transactions and account numbers and get peoples name and license_plates
 SELECT bank_accounts.person_id, people.name, people.license_plate FROM bank_accounts JOIN people ON bank_accounts.person_id = people.id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = 'Leggett Street');
 -- CHECK bakery security logs for license_plate and time between 10:15 and 10:25
