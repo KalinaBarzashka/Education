3a7aee9a22e3946f5dc0ec3f314760f834d7e654
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-16 08:46:59 +0000

    Tue, Aug 16, 2022, 8:46 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,3 +8,5 @@ SELECT id, account_number, transaction_type, amount FROM atm_transactions WHERE 
 SELECT bank_accounts.person_id, people.name, people.license_plate FROM bank_accounts JOIN people ON bank_accounts.person_id = people.id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw');
 -- CHECK bakery security logs for license_plate and time between 10:15 and 10:25
 SELECT activity, license_plate, hour, minute FROM bakery_security_logs WHERE year = 2021 AND month = 07 AND day = 28 AND hour = 10 AND minute BETWEEN 15 AND 25;
+--CHECK phone calls
+SELECT caller, receiver, duration FROM phone_calls WHERE  year = 2021 AND month = 07 AND day = 28 AND duration < 60;
