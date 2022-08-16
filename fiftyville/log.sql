81a6fb19ef00cd8f33501b6a9dfb37350073bb2a
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-16 08:25:12 +0000

    Tue, Aug 16, 2022, 8:25 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
--- Keep a log of any SQL queries you execute as you solve the mystery.
+-- SELECT id and description of crime scene reports - found out how many intervies were taken
 SELECT id, description FROM crime_scene_reports WHERE year = 2021 AND month = 07 AND day = 28 AND street = 'Humphrey Street';
+-- Find more information from the interviews
+SELECT id, name, transcript FROM interviews WHERE year = 2021 AND month = 07 AND day = 28;
