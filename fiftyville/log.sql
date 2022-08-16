c6488bafaa56cd151ac73a000966b53d1ba6fc1e
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-16 08:59:25 +0000

    Tue, Aug 16, 2022, 8:59 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -16,3 +16,8 @@ SELECT caller, receiver, people.name, people.passport_number FROM phone_calls JO
 SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 07 AND day = 28 AND hour = 10 AND minute BETWEEN 15 AND 25 INTERSECT SELECT people.license_plate FROM phone_calls JOIN people ON phone_calls.caller = people.phone_number WHERE  year = 2021 AND month = 07 AND day = 28 AND duration < 60;
 --Intersect prev license plates and with those from the ATM bank account - people - license_plate; found 2 total 322W7JE AND 94KL13X
 SELECT people.license_plate FROM bank_accounts JOIN people ON bank_accounts.person_id = people.id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') INTERSECT SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 07 AND day = 28 AND hour = 10 AND minute BETWEEN 15 AND 25 INTERSECT SELECT people.license_plate FROM phone_calls JOIN people ON phone_calls.caller = people.phone_number WHERE  year = 2021 AND month = 07 AND day = 28 AND duration < 60;
+--Get passport numbers of found people license plates - 3592750733 and 5773159633
+SELECT passport_number FROM people WHERE license_plate IN ('322W7JE', '94KL13X');
+--GET 
+--Query flights
+SELECT id, origin_airport_id, destination_airport_id, hour, minute FROM flights WHERE year = 2021 AND month = 07 AND day = 29 ORDER BY hour, minute;
