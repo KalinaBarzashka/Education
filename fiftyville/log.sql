23c54e3cf325ffb82eae78241ac05dfcdc0372c4
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-16 09:09:02 +0000

    Tue, Aug 16, 2022, 9:09 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -18,6 +18,15 @@ SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 07 
 SELECT people.license_plate FROM bank_accounts JOIN people ON bank_accounts.person_id = people.id WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 07 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw') INTERSECT SELECT license_plate FROM bakery_security_logs WHERE year = 2021 AND month = 07 AND day = 28 AND hour = 10 AND minute BETWEEN 15 AND 25 INTERSECT SELECT people.license_plate FROM phone_calls JOIN people ON phone_calls.caller = people.phone_number WHERE  year = 2021 AND month = 07 AND day = 28 AND duration < 60;
 --Get passport numbers of found people license plates - 3592750733 and 5773159633
 SELECT passport_number FROM people WHERE license_plate IN ('322W7JE', '94KL13X');
---GET 
---Query flights
-SELECT id, origin_airport_id, destination_airport_id, hour, minute FROM flights WHERE year = 2021 AND month = 07 AND day = 29 ORDER BY hour, minute;
+--GET Fiftyville airport id
+SELECT id FROM airports WHERE city = 'Fiftyville';
+--Query flights from Fiftyville airport id - get the first in the morning - id 36; destination_airport_id = 4
+SELECT id, destination_airport_id, hour, minute FROM flights WHERE year = 2021 AND month = 07 AND day = 29 AND origin_airport_id = (SELECT id FROM airports WHERE city = 'Fiftyville') ORDER BY hour, minute LIMIT 1;
+--GET destination city of the theft
+SELECT city FROM airports WHERE id = 4;
+--Get passengers for flight with id 36; search for passport numbers 3592750733 and 5773159633
+SELECT passport_number FROM passengers WHERE flight_id = 36 AND passport_number IN ('3592750733', '5773159633');
+--GET name of person with passport number 5773159633
+SELECT name FROM people WHERE passport_number = '5773159633';
+--The ACCOMPLICE is the one bruce talked with - get phone_number and name
+SELECT receiver, people.name FROM phone_calls JOIN people ON phone_calls.receiver = people.phone_number WHERE year = 2021 AND month = 07 AND day = 28 AND duration < 60 AND caller = (SELECT phone_number FROM people WHERE passport_number = '5773159633');
