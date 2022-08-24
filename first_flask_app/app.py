454146c328d95edaf77e29a171ef76e6d2b68a49
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-24 09:11:59 +0000

    Wed, Aug 24, 2022, 9:11 AM +00:00

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/") # when to call this index function - decorator
 def index():
-    name = request.args.get("name")
-    return render_template("index.html", name=name) # name=name; first name is the name of the variable we want to give to the template and the second is the value
+    #name = request.args.get("name") #name=name in render_template() # name=name; first name is the name of the variable we want to give to the template and the second is the value
+    return render_template("index.html")
