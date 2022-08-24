4e02a4e24600fa6a87e98f7708d3337460f68311
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-24 08:51:00 +0000

    Wed, Aug 24, 2022, 8:51 AM +00:00

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -1,1 +1,7 @@
 import flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/") # when to call this index function
+def index():
+    return render_template("index.html")
