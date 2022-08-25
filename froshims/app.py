39f1fc12f9356f7b2838704bef47beee29d48038
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-25 06:48:45 +0000

    Thu, Aug 25, 2022, 6:48 AM +00:00

--- froshims/app.py
+++ froshims/app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/register". methods=["POST"])
+def register:
+    # Validate submission
+    if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]
+    return render_template("?")
