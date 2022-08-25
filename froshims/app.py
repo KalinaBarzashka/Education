3245917dcaf62b1b06548a1e0db56e4712f021c6
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-25 06:51:21 +0000

    Thu, Aug 25, 2022, 6:51 AM +00:00

--- froshims/app.py
+++ froshims/app.py
@@ -11,5 +11,8 @@ def index():
 @app.route("/register". methods=["POST"])
 def register:
     # Validate submission
-    if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]
-    return render_template("?")
+    if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]:
+        return render_template("failure.html")
+
+    # Confirm registration
+    return render_template("success.html")
