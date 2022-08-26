50b27b2388e74eae44df9b7beeea366bd1999846
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-26 18:23:16 +0000

    Fri, Aug 26, 2022, 6:23 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -140,7 +140,7 @@ def register():
         if len(rows) != 0:
             return apology("username already taken", 403)
 
-        hash = generate_password_hash(password, "sha256", "MAGIC UN")
+        hash = generate_password_hash(password, "sha256")
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
 
         # Redirect user to login page
