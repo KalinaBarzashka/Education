3dec8ba2e810e6d343c43ccc85787a6ffe381539
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-27 19:34:35 +0000

    Sat, Aug 27, 2022, 7:34 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -46,8 +46,13 @@ def index():
     """Show portfolio of stocks"""
 
     data = db.execute("SELECT symbol, COUNT(shares) as shares, AVG(share_price) as share_price FROM transactions WHERE user_id = ? GROUP BY user_id, symbol", session["user_id"])
-    count = data.scalar()
-    return render_template("test.html", data=data, count=count)
+    for d in data:
+        d["name"] = lookup(d["symbol"])["name"]
+        d["total_price"] = d["shares"] * d["share_price"]
+
+    current_user_price = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+
+    return render_template("test.html", data=data, current_user_price=current_user_price)
 
 
 @app.route("/buy", methods=["GET", "POST"])
