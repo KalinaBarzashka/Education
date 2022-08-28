402faace446f746136c2e90d65408ab94287fb19
Kalina Barzashka <57636040+KalinaBarzashka@users.noreply.github.com>
2022-08-28 08:43:02 +0000

    Sun, Aug 28, 2022, 8:43 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -232,8 +232,7 @@ def sell():
 
         current_user_shares = db.execute("SELECT SUM(shares) as sum FROM transactions WHERE user_id = ? and symbol = ? GROUP BY user_id, symbol", session["user_id"], symbol)
 
-        count = current_user_shares.count
-        if count <= 0:
+        if not current_user_shares:
             return apology("you does not own that much stocks", 403)
 
         if current_user_shares[0]["sum"] < int(shares):
