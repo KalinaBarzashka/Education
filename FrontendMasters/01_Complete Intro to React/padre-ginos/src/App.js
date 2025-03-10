c5fcd7e35314e79ac9719839b17a3a9f0729d982
KalinaBarzashka <kalina.barzashka@gmail.com>
2025-03-10 23:03:29 +0200

    add vite

--- FrontendMasters/01_Complete Intro to React/padre-ginos/src/App.js
+++ FrontendMasters/01_Complete Intro to React/padre-ginos/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 // create out first app component here:
 const Pizza = (props) => {
   return React.createElement("div", {}, [
