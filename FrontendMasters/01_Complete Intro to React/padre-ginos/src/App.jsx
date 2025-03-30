6b037658557d483fa0e0a6380fe6f5e8593d1344
KalinaBarzashka <kalina.barzashka@gmail.com>
2025-03-30 21:19:11 +0300

    add hooks

--- FrontendMasters/01_Complete Intro to React/padre-ginos/src/App.jsx
+++ FrontendMasters/01_Complete Intro to React/padre-ginos/src/App.jsx
@@ -1,19 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return(
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="The Pepperoni Pizza"
-        description="Mozzarella Cheese, Pepperoni"
-        image={"/public/pizzas/pepperoni.webp"} />
-      <Pizza name="The Barbecue Chicken Pizza"
-        description="Barbecued Chicken, Red Peppers, Green Peppers, Tomatoes, Red Onions, Barbecue Sauce"
-        image={"/public/pizzas/bbq_ckn.webp"} />
-      <Pizza name="The California Chicken Pizza"
-        description="Chicken, Artichoke, Spinach, Garlic, Jalapeno Peppers, Fontina Cheese, Gouda Cheese"
-        image={"/public/pizzas/cali_ckn.webp"} />
+      <Order />
     </div>
   )
 };
